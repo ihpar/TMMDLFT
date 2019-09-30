b895d1414b9a16ea2d29d1b433f952055991b8d3
ihpar <ismail@cs.deu.edu.tr>
2019-09-30 15:05:20 +0300

    nakarat finding to mu2

--- src/nakarat_finder.py
+++ src/nakarat_finder.py
@@ -118,12 +118,46 @@ def build_corpus(makam):
         i += 1
 
 
+def place_nakarat(makam, edge, coords, song):
+    song_path = os.path.join(os.path.abspath(__file__ + "/../../"), 'songs', makam, song + '.mu2')
+    length = count_len(song_path)
+    in_edge = math.ceil(math.sqrt(length))
+    offset = math.floor((edge - in_edge) / 2)
+    c_top, c_left, c_width, c_height = coords[0], coords[2], coords[2], coords[3]
+    start_idx = (c_top - offset) * in_edge + c_left
+    end_idx = (c_top + c_height - offset) * in_edge + c_width
+    print(start_idx, end_idx)
+
+    rows_list = []
+    nom_index = 3
+    den_index = 4
+    i = 0
+    with codecs.open(song_path, 'r', encoding='utf8') as sf:
+        lines = sf.read().splitlines()
+        for line in lines:
+            rows_list.append(line)
+            parts = line.split('\t')
+            if int(parts[0]) == 9:
+                if parts[nom_index].isdigit() and parts[den_index].isdigit():
+                    if i == start_idx:
+                        rows_list.append('9							[		0.5')
+                    if i == end_idx:
+                        rows_list.append('9								]	0.5')
+                    i += 1
+
+    song_path = os.path.join(os.path.abspath(__file__ + "/../../"), 'songs', makam, 'n_' + song + '.mu2')
+    with codecs.open(song_path, 'w', encoding='utf8') as sf:
+        for line in rows_list:
+            sf.write(line + '\n')
+
+
 def main():
     makam = 'hicaz'
-    build_corpus(makam)
-
-    # region for new guesses
+    # build_corpus(makam)
     edge = 26  # hicaz
+
+    '''
+    # region for new guesses
     maxi = 404
     note_dict = NCDictionary()
     oh_manager = OhManager(makam)
@@ -133,6 +167,10 @@ def main():
     im = Image.fromarray(notes)
     im = im.convert('L')
     im.save(os.path.join(os.path.abspath(__file__ + "/../../"), 'songs', makam, 'lstm_v61_88.png'))
+    '''
+    # [10  6 14  4] <-> [top left width height] : lstm_v61_66.png
+    coords = np.array([10, 6, 14, 4])
+    place_nakarat(makam, edge, coords, 'lstm_v61_66')
 
 
 if __name__ == '__main__':
