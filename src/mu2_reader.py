817d96e6e37a877985377601898012ec1c6a29b2
ihpar <ismail@cs.deu.edu.tr>
2019-10-17 16:48:51 +0300

    new logix

--- src/mu2_reader.py
+++ src/mu2_reader.py
@@ -1,17 +1,20 @@
 import codecs
 from os import path
 from fractions import Fraction
+
+from nc_dictionary import NCDictionary
+from oh_manager import OhManager
 from songobj import SongObj
 
 
-def decompose_mu2(dp, fn, part_map):
+def decompose_mu2(dp, fn, part_map, note_dict, oh_manager):
     nom_index = 2
     den_index = 3
     beat, tot = None, Fraction(0)
     measure_no = 0
-    I, A, B, C = part_map['I'], part_map['A'], part_map['B'], part_map['C']
     song_path = path.join(dp, fn)
     song = SongObj(fn)
+    song.init_measures(part_map)
     with codecs.open(song_path, 'r', encoding='utf8') as sf:
         lines = sf.read().splitlines()
         for line in lines:
@@ -26,19 +29,38 @@ def decompose_mu2(dp, fn, part_map):
                 song.set_tempo(int(parts[4]))
 
             elif parts[0] == 9 and (parts[nom_index].isdigit() and parts[den_index].isdigit()):
+                # note name
+                note_name = parts[1].lower().strip()
+                if note_name == '':
+                    note_name = 'rest'
+                note_num = note_dict.get_note_by_name(note_name)
+                # note dur
                 note_len = Fraction(int(parts[nom_index]), int(parts[den_index]))
+                dur = str(note_len)
+                dur_alt = parts[nom_index] + '/' + parts[den_index]
+                dur = note_dict.get_num_by_dur(dur)
+                if not dur:
+                    dur = note_dict.get_num_by_dur(dur_alt)
+                # repr
+                combine = oh_manager.nd_2_int(str(note_num) + ':' + str(dur))
+                song.insert_note(combine, measure_no)
+
                 tot += note_len
                 if tot == beat:
                     measure_no += 1
                     tot = Fraction(0)
-                    print(parts)
+
+    song.print_self()
 
 
 def main():
+    makam = 'hicaz'
     dir_path = 'C:\\Users\\istir\\Desktop\\SymbTr-master\\mu2'
     song = 'hicaz--sarki--agiraksak--bak_ne--haci_arif_bey.mu2'
     part_map = {'I': [0, 1, 2, [3, 4]], 'A': [5, 6, 7, 8], 'B': [9, 10, 11, 12], 'C': [13, 14, 15, 16]}
-    decompose_mu2(dir_path, song, part_map)
+    note_dict = NCDictionary()
+    oh_manager = OhManager(makam)
+    decompose_mu2(dir_path, song, part_map, note_dict, oh_manager)
 
 
 if __name__ == '__main__':
--- src/songobj.py
+++ src/songobj.py
@@ -1,15 +1,57 @@
+class Measure:
+    def __init__(self, m_no, rep=False):
+        self._no = m_no
+        self._rep = rep
+        self._notes = []
+
+    def print_self(self):
+        print(self._no, self._rep, self._notes)
+
+
+class PartObj:
+    def __init__(self, pid, m_list):
+        self._pid = pid
+        self._measures = []
+        for e in m_list:
+            if isinstance(e, list):
+                self._measures.append(Measure(e[0], True))
+                self._measures.append(Measure(e[1], True))
+            else:
+                self._measures.append(Measure(e))
+
+    def print_self(self):
+        print(self._pid)
+        for measure in self._measures:
+            measure.print_self()
+
+
 class SongObj:
     def __init__(self, song_name, tempo=None, time_signature=None):
         self._name = song_name
         self._tempo = tempo
         self._time_sign = time_signature
-        self._I = []
-        self._A = []
-        self._B = []
-        self._C = []
+        self._parts = []
 
     def set_tempo(self, tempo):
         self._tempo = tempo
 
     def set_time_sign(self, time_signature):
         self._time_sign = time_signature
+
+    def init_measures(self, parts_map):
+        for k in parts_map:
+            self._parts.append(PartObj(k, parts_map[k]))
+
+    def in_list(self, my_list, item):
+        if item in my_list:
+            return True
+        else:
+            return any(self.in_list(sublist, item) for sublist in my_list if isinstance(sublist, list))
+
+    def print_self(self):
+        print(self._name, self._time_sign, self._tempo)
+        for part in self._parts:
+            part.print_self()
+
+    def insert_note(self, note, curr_m_no):
+        pass
