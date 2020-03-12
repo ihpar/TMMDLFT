580bd9092bf15f84f8eb5c71f279d6abd95a29ee
ihpar <ismail@cs.deu.edu.tr>
2020-03-12 16:52:10 +0300

    nihavent

--- src/nihavent_parts.py
+++ src/nihavent_parts.py
@@ -1,5 +1,9 @@
 nihavent_songs = [
-    {'file': 'hicaz--sarki--agiraksak--bak_ne--haci_arif_bey.mu2',
-     'parts_map': {'I': [], 'A': [], 'B': [], 'C': []},
+    {'file': 'nihavent--ek--aglamakla--inlemekle.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3, 4, 5, [6, 7]], 'B': [8, 9, 10, 11, 12, 13, [14, 15]], 'C': [16, 17, 18, 19, 20, 21, 22, 23]},
+     'sf': []},
+
+    {'file': 'nihavent--ek--aksami-getiren.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, [3, 4]], 'B': [5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 'C': [15, 16, 17, 18, 19, 20, 21, 22]},
      'sf': []}
 ]
