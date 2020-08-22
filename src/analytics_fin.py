d40b47702f458009282c215d48a2ef69da438eae
ihpar <ismail@cs.deu.edu.tr>
2020-08-23 00:16:37 +0300

    nihavent

--- src/analytics_fin.py
+++ src/analytics_fin.py
@@ -2,8 +2,11 @@ import os
 import numpy as np
 import codecs
 from fractions import Fraction
+from sklearn.model_selection import LeaveOneOut
+import sys
 
 import hicaz_parts
+import nihavent_parts
 from dur_translator import DurTranslator
 from nc_dictionary import NCDictionary
 from note_translator import NoteTranslator
@@ -50,10 +53,13 @@ def parse_song_in_mu2(song_path, note_dict, nt=None, dt=None):
 
 
 def get_base_data(makam, note_dict=None, nt=None, dt=None):
+    min_song_len = sys.maxsize
+    broad_list = []
     note_nums, dur_nums = [], []
     with open(makam + '--nc_corpus.txt', 'r') as crp:
         songs = crp.read().splitlines()
         for song in songs:
+            song_entity = {'notes': [], 'durs': []}
             notes = song.split(' ')
             for note in notes:
                 parts = [int(x) for x in note.split(':')]
@@ -68,39 +74,64 @@ def get_base_data(makam, note_dict=None, nt=None, dt=None):
                 note_nums.append(parts[0])
                 dur_nums.append(parts[1])
 
+                song_entity['notes'].append(parts[0])
+                song_entity['durs'].append(parts[1])
+
+            min_song_len = min(min_song_len, len(song_entity['notes']))
+            broad_list.append(song_entity)
+
     added_songs = []
     if makam == 'hicaz':
         for song in hicaz_parts.hicaz_songs:
             if song['file'].startswith('bes-hicaz-'):
                 added_songs.append(song['file'])
+    elif makam == 'nihavent':
+        for song in nihavent_parts.nihavent_songs:
+            added_songs.append(song['file'])
 
     for song in added_songs:
         song = os.path.join(os.path.abspath('..'), 'songs', 'added', makam, song)
         song = parse_song_in_mu2(song, note_dict, nt, dt)
+        song_entity = {'notes': [], 'durs': []}
+
         for note in song:
             parts = [int(x) for x in note.split(':')]
             note_nums.append(parts[0])
             dur_nums.append(parts[1])
+            song_entity['notes'].append(parts[0])
+            song_entity['durs'].append(parts[1])
+
+        min_song_len = min(min_song_len, len(song_entity['notes']))
+        broad_list.append(song_entity)
 
     note_nums = np.array(note_nums)
     dur_nums = np.array(dur_nums)
-    return note_nums, dur_nums
+    return note_nums, dur_nums, broad_list, min_song_len
 
 
+# noinspection DuplicatedCode
 def get_gen_data(generated_songs_path, note_dict=None, nt=None, dt=None):
+    min_song_len = sys.maxsize
+    broad_list = []
+
     note_nums, dur_nums = [], []
     gen_songs = [os.path.join(generated_songs_path, f) for f in os.listdir(generated_songs_path) if os.path.isfile(os.path.join(generated_songs_path, f))]
     for song in gen_songs:
         song = parse_song_in_mu2(song, note_dict, nt, dt)
-
+        song_entity = {'notes': [], 'durs': []}
         for note in song:
             parts = [int(x) for x in note.split(':')]
             note_nums.append(parts[0])
             dur_nums.append(parts[1])
+            song_entity['notes'].append(parts[0])
+            song_entity['durs'].append(parts[1])
+
+        min_song_len = min(min_song_len, len(song_entity['notes']))
+        broad_list.append(song_entity)
 
     note_nums = np.array(note_nums)
     dur_nums = np.array(dur_nums)
-    return note_nums, dur_nums
+    return note_nums, dur_nums, broad_list, min_song_len
 
 
 def abs_measurement(makam, generated_songs_path):
@@ -112,21 +143,36 @@ def abs_measurement(makam, generated_songs_path):
         dt = DurTranslator(makam)
 
     # oh_manager = OhManager(makam)
-    note_nums_src, dur_nums_src = get_base_data(makam, note_dict, nt, dt)
+    note_nums_src, dur_nums_src, broad_list, min_len = get_base_data(makam, note_dict, nt, dt)
     mean_src = np.mean(note_nums_src)
     std_src = np.std(note_nums_src)
     print('src mean:', mean_src, 'src std:', std_src)
 
-    note_nums_gen, dur_nums_gen = get_gen_data(generated_songs_path, note_dict, nt, dt)
+    note_nums_gen, dur_nums_gen, gen_broad_list, gen_min_len = get_gen_data(generated_songs_path, note_dict, nt, dt)
     mean_gen = np.mean(note_nums_gen)
     std_gen = np.std(note_nums_gen)
-    print('src mean:', mean_gen, 'src std:', std_gen)
+    print('gen mean:', mean_gen, 'gen std:', std_gen)
+
+
+def exhaustive_cross_val_inter():
+    num_samples = 100
+
+    set1_eval = {'total_used_pitch': np.zeros((num_samples, 1))}
+    metrics_list = set1_eval.keys()
+
+    loo = LeaveOneOut()
+    loo.get_n_splits(np.arange(num_samples))
+
+    set1_intra = np.zeros((num_samples, len(metrics_list), num_samples - 1))
+    set2_intra = np.zeros((num_samples, len(metrics_list), num_samples - 1))
 
 
 def main():
-    makam = 'hicaz'
-    generated_songs_path = os.path.join(os.path.abspath('..'), 'songs', 'hicaz-sarkilar')
-    abs_measurement(makam, generated_songs_path)
+    makam = 'nihavent'
+    gen_dir = 'nihavent'
+    generated_songs_path = os.path.join(os.path.abspath('..'), 'songs', gen_dir)
+    # abs_measurement(makam, generated_songs_path)
+    exhaustive_cross_val_inter()
 
 
 if __name__ == '__main__':
