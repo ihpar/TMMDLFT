a58d392e894912b10c1fce67a5f7f59357892d7c
ihpar <ismail@cs.deu.edu.tr>
2020-09-15 16:38:31 +0300

    stats

--- src/nihavent_gen_parts.py
+++ src/nihavent_gen_parts.py
@@ -1,5 +1,81 @@
 nihavent_gen_songs = [
     {'file': 'Nihavent_Duyek_Tester_0.mu2',
      'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, 7], 'C': [8, 9, 10, 11]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_1.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_2.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_3.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_4.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_5.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_6.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, 7], 'C': [8, 9, 10, 11]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_7.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, 7], 'C': [8, 9, 10, 11]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_8.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_9.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_10.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, 7], 'C': [8, 9, 10, 11]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_11.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_12.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_13.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_14.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_15.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_16.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_17.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, [7, 8]], 'C': [9, 10, 11, 12]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_18.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, 7], 'C': [8, 9, 10, 11]},
+     'sf': []},
+
+    {'file': 'Nihavent_Duyek_Tester_19.mu2',
+     'parts_map': {'I': [], 'A': [0, 1, 2, 3], 'B': [4, 5, 6, 7], 'C': [8, 9, 10, 11]},
      'sf': []}
 ]
