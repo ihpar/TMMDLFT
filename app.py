6e022c02d4062267d5138d6bd900c1af21cdb755
ihpar <ismail@cs.deu.edu.tr>
2021-01-14 22:36:35 +0300

    ui support

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, url_for
 from flask_socketio import SocketIO, send, emit
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lorem_ip'
 socketio = SocketIO(app)
 
 
-@socketio.on('client_connected')
-def handle_client_connect_event(data):
-    print("yay! client connected")
-    print('data is:', str(data))
+@socketio.on('start_composing')
+def start_composing(data):
+    makam = data['makam']
+    notes = data['notes']
+    print(makam)
+    for combo in notes:
+        parts = combo.split(':')
+        note = parts[0]
+        dur = parts[1]
+        print(note, dur)
+
+    emit('composition_status', {'det': 1})
+    time.sleep(4)
+    emit('composition_status', {'ket': ['a', 2, 3.14]})
+    time.sleep(4)
+    emit('composition_status', {'suc': {'met': -3}})
 
 
 @socketio.on('disconnect')
@@ -33,4 +46,4 @@ def index():
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
